32373d21176bd3e7dd4c1b72257cfbe885236982
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-05-16 13:56:47 +0300

    fetch contract abi

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 import Web3 from 'web3';
+import {contractAbi, contractAddress } from './utils/constants';
+import {useEffect, useState } from 'react';
+import './App.css';
 
-function App() {
+
+const web3 = new Web3("ws://localhost:8545")
+const greeterContract = new web3.eth.Contract(contractAbi, contractAddress);
+
+const App = () => {
+  const [newGreetings, setNewGreetings] = useState("");
+  const [greetings, setGreetings] = useState("");
+
+  useEffect(() => async() => {
+    const greetMsg = await greetMe()
+    setGreetings(greetMsg);
+  },[])
+
+  const greetMe = async () => {
+    const greetMsg = await greeterContract.methods.greet().call();
+    return greetMsg;
+  }
+  const updateGreets = async () => {
+    const greetMsg = await greeterContract.methods.setGreeting(newGreetings).send(
+      {from: '0xde9be858da4a475276426320d5e9262ecfc3ba460bfac56360bfa6c4c28b4ee0'}
+    )
+    setGreetings(await greetMe())
+  }
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <input placeholder="New Greetings" type="text" value={newGreetings} onCHange={(e) => setNewGreetings(e.target.value)}/>
+    <button onClick={() => updateGreets()}>
+      Update Greetings
+    </button>
+    <h2>Current Greetings:
+      <span style={{color: "blueviolet"}}>
+        {greetings}
+      </span>
+    </h2>
     </div>
+    </>
   );
 }
 
---
+++ my-project/src/utils/constants.js
@@ -0,0 +1,4 @@
+import Greeter from './Greeter.json';
+
+export const contractAbi = Greeter.abi;
+export const contractAddress = '0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e';
